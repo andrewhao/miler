2c1ee021d9dc86c8ced1827f09ca4445693328af
Andrew Hao <andrewhao@gmail.com>
2015-04-13 07:34:23 -0700

    1st commit with trial usage of the API.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "dailymile-ruby"
+gem "thor"
+gem "pry"
+gem "pry-byebug"
+gem "awesome_print"
---
+++ miler.rb
@@ -0,0 +1,32 @@
+require 'dailymile'
+require 'thor'
+require 'pry'
+require 'pry-byebug'
+require 'awesome_print'
+
+class Miler < Thor
+  desc 'download USERNAME', "Download GPX for USERNAME"
+  def download(username)
+    res = client.get "/people/#{username}/routes"
+    binding.pry
+    ap res
+  end
+
+  desc 'gpx ROUTEID', "Download route ROUTEID"
+  def gpx(route_id)
+    gpx = client.get "/routes/#{route_id}.gpx"
+    binding.pry
+    puts gpx
+  end
+
+  private
+
+  def client
+    @client ||= begin
+                  Dailymile::Client.set_client_credentials ENV['DM_CLIENT_ID'], ENV['DM_CLIENT_SECRET']
+                  Dailymile::Client.new ENV['DM_DEV_ACCESS_TOKEN']
+                end
+  end
+end
+
+Miler.start(ARGV)
